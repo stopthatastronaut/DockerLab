efc34bee89cdac1c43fe97ff74af7688a352f4a9
Jason Brown <stopthatastronaut@gmail.com>
2021-05-12 16:16:07 +1000

    Add a dummy cotnainer for testing automation stuff

---
+++ Pwsh-DockerHelper/DockerHelper.ps1
@@ -0,0 +1,78 @@
+﻿# Necessarily Incomplete thus far
+
+Function Remove-Container {
+    [CmdletBinding()]
+    param(
+        [Switch]$Stopped,
+        [String]$Filter = "*",
+        [String]$imageId
+    )
+
+    <# docker container rm $(docker container ls –aq) #>
+    <# docker container prune #>
+
+    $splat = [pscustomobject]@{ Filter = $Filter }
+    if ($Stopped) { $splat | Add-Member -Name Stopped -Value $true -MemberType NoteProperty }
+
+    Write-Verbose $splat
+
+    $containers = Get-Container @splat
+
+    Write-Verbose ("Remove-Container Found " + ($containers | measure | select -expand Count) + " containers")
+
+    $containers | % {
+        Write-Verbose "removing docker container $($_.names) ($($_.ID))"
+        docker rm $_.id
+    }
+}
+
+
+Function Get-Container {
+    [CmdletBinding()]
+    param(
+        [Switch]$Stopped,
+        [String]$Filter = "*"
+    )
+    $cimages = Get-Image # a dependency for injection so we don't smash the api
+
+    $statefilter = if ($Stopped) {
+        "exited"
+    }
+    else {
+        "*"
+    }
+
+    return docker ps -a --format '{{json .}}' |
+    ConvertFrom-Json | % {
+        $_ | Add-Member -name "imagename" -Value (Get-ImageName $_.image $cimages) -MemberType NoteProperty -PassThru
+    } |
+    Where-Object { $_.imagename -like $Filter } |
+    Where-Object { $_.state -like $statefilter }
+
+}
+
+Function Get-ImageName {
+    [CmdletBinding()]
+    param($id, $imagelist)
+    Write-Verbose ("resolving image $id from a list of " + ($imagelist | measure-object |  select-object -expand count) + " images")
+    $imagename = $imagelist |
+    Where-Object { $_.id -eq $id } |
+    Select-Object -first 1 | # in case we have multiple tagged images with the same ID
+    Select-Object -expand Repository
+    Write-Verbose "Resolved image name $imagename"
+    return $imagename
+}
+
+Function Remove-Image {
+    [CmdletBinding()]
+    param($prefix, $imageId)
+}
+
+Function Get-Image {
+    return docker image ls --format '{{json .}}' | ConvertFrom-Json
+}
+
+Function Stop-Image {
+    # what's this doing here?
+
+}
---
+++ Pwsh-DockerHelper/DockerHelper.tests.ps1
@@ -0,0 +1,60 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Get-Container" {
+    It "Gets a list of containers in powershell object format" {
+        Get-Container | Should Not Be $null
+        (Get-Container).TypeName
+    }
+
+    It "Adds an 'imagename' column" {
+        Get-Container | Select -expand imagename | Should not be $null
+    }
+
+    It "Only gets exited containers when asked for '-stopped'" {
+
+    }
+}
+
+Describe "Get-Image" {
+    It "Can get a list of images" {
+
+    }
+
+    Mock docker {
+        param()
+
+    }
+
+    It "Can filter images by name" {
+
+    }
+}
+
+Describe "Get-ImageName" {
+    $g = (new-guid | select -expand Guid) -split "-" | select -last 1
+
+    Mock Get-Image {
+        return @(
+            @{Repository = "stripe-report-runner"; ID = $g }
+            @{Repository = "stripe-notyour-image"; ID = "c4c142da0bff" }
+            @{Repository = "stripe-notyour-image"; ID = "c4c1bbda0bff" }
+            @{Repository = "stripe-report-runner"; ID = "1234456789ee" }
+            @{Repository = "stripe-notyour-image"; ID = "c4c142da0bff" }
+        )
+    }
+
+    It "Can resolve an image name from an ID" { # temp test
+        $imagelist = Get-Image
+        Get-ImageName -id $g $imagelist | Should Be "stripe-report-runner"
+    }
+}
+
+Describe "Remove-Container" {
+
+}
+
+Describe "Remove-Image" {
+
+}
